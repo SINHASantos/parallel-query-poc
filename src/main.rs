10a1034efd3befcf5670f6c3ade7a5d2b095b5e8
Andy Grove <andygrove73@gmail.com>
2019-04-19 20:32:54 -0600

    comments

--- src/main.rs
+++ src/main.rs
@@ -39,18 +39,30 @@ fn main() {
     }
 }
 
+/// An execution plan produces one iterator over record batches per partition
 trait ExecutionPlan {
     fn execute(&self) -> Vec<Arc<Mutex<ThreadSafeRecordBatchIterator>>>;
 }
 
+/// Iterator for reading a series of record batches with a known schema
+pub trait ThreadSafeRecordBatchIterator: Send {
+    /// Get the schema of this iterator
+    fn schema(&self) -> &Arc<Schema>;
+
+    /// Get the next batch in this iterator
+    fn next(&self) -> Result<Option<RecordBatch>>;
+}
+
+/// Execution plan for reading partitioned parquet files
 struct ParquetExec {
-    filename: String,
+    /// Directory containing parquet files with identical schemas (one file = one partition)
+    dir: String,
 }
 
 impl ParquetExec {
     pub fn new(filename: &str) -> Self {
         Self {
-            filename: filename.to_string(),
+            dir: filename.to_string(),
         }
     }
 }
@@ -58,9 +70,9 @@ impl ParquetExec {
 impl ExecutionPlan for ParquetExec {
     fn execute(&self) -> Vec<Arc<Mutex<ThreadSafeRecordBatchIterator>>> {
         let mut parquet_partitions: Vec<Arc<Mutex<ThreadSafeRecordBatchIterator>>> = vec![];
-        for entry in fs::read_dir(&self.filename).unwrap() {
+        for entry in fs::read_dir(&self.dir).unwrap() {
             let entry = entry.unwrap();
-            let filename = format!("{}/{}", &self.filename, entry.file_name().to_str().unwrap());
+            let filename = format!("{}/{}", &self.dir, entry.file_name().to_str().unwrap());
             println!("{}", filename);
             let parquet_channel = ParquetChannel::open(&filename);
             parquet_partitions.push(Arc::new(Mutex::new(parquet_channel)));
@@ -141,15 +153,6 @@ impl ParquetChannel {
             response_rx,
         }
     }
-}
-
-/// Iterator for reading a series of record batches with a known schema
-pub trait ThreadSafeRecordBatchIterator: Send {
-    /// Get the schema of this iterator
-    fn schema(&self) -> &Arc<Schema>;
-
-    /// Get the next batch in this iterator
-    fn next(&self) -> Result<Option<RecordBatch>>;
 }
 
 impl ThreadSafeRecordBatchIterator for ParquetChannel {
